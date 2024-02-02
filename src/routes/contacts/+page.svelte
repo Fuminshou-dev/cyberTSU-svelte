bf99de6dc8e1bc22250ed65dcacaf38dae0cb5b9
Fuminshou <petrusman0@gmail.com>
2024-02-02 13:01:12 +0700

    small changes on contacts page

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,33 +1,79 @@
-<div class="flex justify-center items-center self-center h-full w-dvw">
-  <div class="">
-    <div>
-      <div class="chat chat-start">
-        <div class="chat-bubble chat-bubble-primary">
-          Хочешь связаться с нами?
-        </div>
-      </div>
-      <div class="chat chat-start">
-        <div class="chat-bubble chat-bubble-primary">
-          В таком случае, тебе необходимо обратиться на почту или в вк :)
-        </div>
-      </div>
-      <div class="chat chat-end">
-        <div class="chat-bubble chat-bubble-info">А ссылочку???</div>
-      </div>
+<div
+  class="justify-center hidden md:flex items-center self-center h-full w-dvw select-none"
+>
+  <h1 class="break-words pl-5">
+    <div class="mockup-phone">
+      <div class="camera"></div>
+      <div class="display">
+        <div class="artboard artboard-demo phone-1">
+          <div class="">
+            <div>
+              <div class="chat chat-start">
+                <div class="chat-bubble chat-bubble-primary">
+                  Хочешь связаться с нами?
+                </div>
+              </div>
+              <div class="chat chat-start">
+                <div class="chat-bubble chat-bubble-primary">
+                  В таком случае, тебе необходимо обратиться на почту или в вк
+                  :)
+                </div>
+              </div>
+              <div class="chat chat-end">
+                <div class="chat-bubble chat-bubble-info">А ссылочку???</div>
+              </div>
 
-      <div class="chat chat-start">
-        <div class="chat-bubble chat-bubble-warning">
-          Точно! <p class="font-bold">Почта:</p>
-          <p class="underline">example@example.com</p>
-          <p class="font-bold">Вк:</p>
-          <a href="https://vk.com/" class="underline" target="_blank">
-            vk.com/??
-          </a>
+              <div class="chat chat-start">
+                <div class="chat-bubble chat-bubble-warning">
+                  Точно! <p class="font-bold">Почта:</p>
+                  <p class="underline select-all">example@example.com</p>
+                  <p class="font-bold">Вк:</p>
+                  <a href="https://vk.com/" class="underline" target="_blank">
+                    vk.com/??
+                  </a>
+                </div>
+              </div>
+              <div class="chat chat-end">
+                <div class="chat-bubble chat-bubble-secondary">Спасибо!</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="chat chat-end">
-        <div class="chat-bubble chat-bubble-secondary">Спасибо!</div>
+    </div>
+  </h1>
+</div>
+
+<div
+  class="flex justify-center md:hidden items-center self-center h-full w-dvw m-0 p-0"
+>
+  <div>
+    <div class="chat chat-start">
+      <div class="chat-bubble chat-bubble-primary">
+        Хочешь связаться с нами?
       </div>
+    </div>
+    <div class="chat chat-start">
+      <div class="chat-bubble chat-bubble-primary">
+        В таком случае, тебе необходимо обратиться на почту или в вк :)
+      </div>
+    </div>
+    <div class="chat chat-end">
+      <div class="chat-bubble chat-bubble-info">А ссылочку???</div>
+    </div>
+
+    <div class="chat chat-start">
+      <div class="chat-bubble chat-bubble-warning">
+        Точно! <p class="font-bold">Почта:</p>
+        <p class="underline select-all">example@example.com</p>
+        <p class="font-bold">Вк:</p>
+        <a href="https://vk.com/" class="underline" target="_blank">
+          vk.com/??
+        </a>
+      </div>
+    </div>
+    <div class="chat chat-end">
+      <div class="chat-bubble chat-bubble-secondary">Спасибо!</div>
     </div>
   </div>
 </div>
